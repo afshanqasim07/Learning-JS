8be53c5eb68464250133c0433a03b97b06d4f2d2
afshanqasim07 <afshaanqasim07@gmail.com>
2024-07-01 09:05:39 -0700

    learning js basics and operators

---
+++ intoduction and operation/app.js
@@ -0,0 +1,74 @@
+console.log("We are Learning JS ")
+// data types 
+// 1.string ("")
+// 2.number (eg 123)
+// 3. boolean (TRUE | FALSE)
+
+
+alert("Welcome Back")
+//  HOW TO MAKE VARIABLES 
+
+var username = "afshanqasim";
+var password =   "afshanqasim123" ;
+var isloggedin= "True" ;
+
+console.log(username , password , isloggedin);
+
+// Operations
+
+//  1. Airthamatic operations
+//  2. assigment operations
+//  3. comparsion operators
+
+
+//  1. Airthamatic operations
+var num = 20;
+var num1 = 50;
+var sum = num +num1 ;
+// var sum = num -num1 ;
+// var sum = num *num1 ;
+// var sum = num /num1 ;
+console.log(sum)
+
+
+//  2. assigment operations
+  var sum1 = 30
+// sum1+=20
+// sum1-=20
+// sum1*=20
+sum1/=20
+console.log(sum1)
+
+
+  //  3. comparsion operators
+
+//   only works in bolean true or false 
+
+
+var zyx= 50; 
+var mno = "50";
+// var Comparision = zyx>mno;
+// var Comparision = mno>zyx;
+// var Comparision = mno ==zyx;
+// var Comparision = mno===zyx;
+// var Comparision = mno!=zyx;
+// var Comparision = mno!==zyx;
+// var Comparision = mno>=zyx;
+var Comparision = mno<=zyx; 
+
+console.log(Comparision);
+// type of is a function which is ued for checking the data type
+typeof(mno)
+    
+var username = prompt("Enter username");
+console.log(username);
+var username = prompt("Enter password");
+console.log(password);
+
+
+
+
+
+
+
+
